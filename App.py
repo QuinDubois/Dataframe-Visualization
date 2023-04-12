1712105c3bdfa031795670c2bc8e082a812e212f
QuinDubois <quindubois@outlook.com>
2023-04-11 22:59:12 -0400

    0.0.0
    First semantic versioning commit

--- App.py
+++ App.py
@@ -10,7 +10,7 @@ app = Dash(
     __name__,
     use_pages=True,
     meta_tags=[
-        {'name': "viewport", 'content': "width=device-width"}
+        {'name': "viewport", 'content': "width=device-width, initial-scale=1"}
     ]
 )
 app.title = "Dashboard"
